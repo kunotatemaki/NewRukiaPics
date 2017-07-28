9c5634d2e09b3428297f79157a60884d76fdd5ed
Kuno Tatemaki <raulfeliz@gmail.com>
2017-07-28 14:43:23 +0200

    create custom scope

---
+++ app/src/main/java/com/rukiasoft/newrukiapics/di/scope/CustomScopes.kt
@@ -0,0 +1,14 @@
+package com.rukiasoft.newrukiapics.di.scope
+
+import javax.inject.Scope
+
+
+/**
+ * Created by Roll on 28/7/17.
+ */
+interface CustomScopes {
+
+    @Scope
+    annotation class ListPicsScope
+
+}
