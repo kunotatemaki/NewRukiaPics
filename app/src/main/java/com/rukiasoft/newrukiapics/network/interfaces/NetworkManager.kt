52d13f039f0a5864bcc41f1e08fc3ee322fbc582
Kuno Tatemaki <raulfeliz@gmail.com>
2017-07-28 10:43:55 +0200

    add flickr endpoints

--- app/src/main/java/com/rukiasoft/newrukiapics/network/interfaces/NetworkManager.kt
+++ app/src/main/java/com/rukiasoft/newrukiapics/network/interfaces/NetworkManager.kt
@@ -1,7 +1,14 @@
 package com.rukiasoft.newrukiapics.network.interfaces
 
+import retrofit2.http.GET
+import retrofit2.http.QueryMap
+
 /**
  * Created by Roll on 28/7/17.
  */
 interface NetworkManager {
+
+    @GET(".")
+    fun Any.getImages(@QueryMap params: Map<String, String>)
+
 }
